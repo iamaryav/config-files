1aa2787eb421a5054e7085b656f2d5a2a632a442
Priya Aryav <priya.aryav@gmail.com>
2026-06-19 23:09:56 +0530

    tab space setting

--- init.lua
+++ init.lua
@@ -31,6 +31,12 @@ end)
 -- Enable break indent
 vim.o.breakindent = true
 
+-- Tab settings
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+
 -- Save undo history
 vim.o.undofile = true
 
